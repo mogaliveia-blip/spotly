1762d11de60cae63966dd4ad699c6dba40295fac
moga live <mogaliveia@gmail.com>
2026-01-07 23:34:49 +0000

    enfin l'upload c'est fait sur storage

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
